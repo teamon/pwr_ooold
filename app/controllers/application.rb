0e12cf327d33df87132ae30ffb47c9a48b40d3fd
Tymon Tobolski <i@teamon.eu>
2009-12-07 16:29:03 +0100

    Date params filter

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,7 +1,13 @@
 class Application < Merb::Controller
   
+  before do
+    if params[:lecture] && params[:lecture][:date]
+      params[:lecture][:date] = params[:lecture][:date].inject({}){|h, (k,v)| h[k] = v.to_i; h } rescue nil
+    end
+  end
+
   protected
-  
+
   def ensure_authenticated
     unless session.authenticated?
       session[:redirect_to] = request.uri
